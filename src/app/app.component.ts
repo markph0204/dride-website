a4ba344d72ac90351f4dc9b25b3e79636a609e0e
Yossi <saoron@gmail.com>
2017-08-09 12:07:42 +0300

    close #37

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-	title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+	routerSubscription: Subscription;
 
 
-	constructor() {
+	constructor(private router: Router) {
 
 	}
 
+	ngOnInit() {
+		if (isPlatformBrowser) {
+			this.routerSubscription = this.router.events
+				.filter(event => event instanceof NavigationEnd)
+				.subscribe(event => {
+					window.scrollTo(0, 0);
+				});
+		}
+	}
+
+	ngOnDestroy() {
+		this.routerSubscription.unsubscribe();
+	}
 
 
 }
